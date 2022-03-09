316c1dc043c4455dff7df607227dd1b71a4a320c
Nicolas Labbe <nicoo.labbe@gmail.com>
2022-03-08 22:44:27 -0300

    creacion de consultas

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,103 +2,81 @@
 
 function Consultorio(nombre, paciente) {
     this.nombre = nombre;
-    this.paciente = paciente;
+    this.paciente = paciente || [];
 }
 
 //función constructora para Pacientes
 function Paciente(nombre, edad, rut, diagnostico) {
-    var _nombre = nombre;
-    var _edad = edad;
-    var _rut = rut;
-    var _diagnostico = diagnostico;
-    Object.defineProperty(this, 'getNombre', {
-        get: function () {
-            return _nombre;
-        }
-    });
-    Object.defineProperty(this, 'setNombre', {
-        set: function (nombre) {
-            _nombre = nombre;
-        }
-    });
-    Object.defineProperty(this, 'getEdad', {
-        get: function () {
-            return _edad;
-        }
-    });
-    Object.defineProperty(this, 'setEdad', {
-        set: function (edad) {
-            _edad = edad;
-        }
-    });
-    Object.defineProperty(this, 'getRut', {
-        get: function () {
-            return _rut;
-        }
-    });
-    Object.defineProperty(this, 'setRut', {
-        set: function (rut) {
-            _rut = rut;
-        }
-    })
-    Object.defineProperty(this, 'getDiagnostico', {
-        get: function () {
-            return _diagnostico;
-        }
-    });
-    Object.defineProperty(this, 'setDiagnostico', {
-        set: function (diagnostico) {
-            _diagnostico = diagnostico;
-        }
-    })
+    this._nombre = nombre;
+    this._edad = edad;
+    this._rut = rut;
+    this._diagnostico = diagnostico;
 }
 
 // Implementar métodos getters y setters para poder acceder y modificar los datos de
 // los pacientes
 Paciente.prototype.getNombre = function () {
-    return this._getnombre;
+    return this._nombre;
 }
 Paciente.prototype.setNombre = function (nombre) {
-    this._setNombre = nombre;
+    this._nombre = nombre;
 }
 Paciente.prototype.getEdad = function () {
-    return this._getedad;
+    return this._edad;
 }
 Paciente.prototype.setEdad = function (edad) {
-    this.setEdad = edad;
+    this._edad = edad;
 }
 Paciente.prototype.getRut = function () {
-    return this._getrut;
+    return this._rut;
 }
 Paciente.prototype.setNombre = function (rut) {
-    this._setRut = rut;
+    this._rut = rut;
 }
 Paciente.prototype.getDiagnostico = function () {
-    return this._getDiagnostico;
+    return this._diagnostico;
 }
 Paciente.prototype.setNombre = function (diagnostico) {
-    this._setDiagnostico = diagnostico;
+    this._diagnostico = diagnostico;
 }
 
+/* pacienteDos._setEdad('56');
+console.log(pacienteDos.getEdad) */
+
+
+//4. Crear un método mediante la propiedad prototype que permita buscar los datos de
+//los usuarios por nombre
+Consultorio.prototype.mostrarPacientes = function () {
+    console.log('Lista de pacientes consultorio Comunal:')
+    for (var i = 0; i < this.paciente.length; i++) {
+        console.log('Nombre; ' + this.paciente[i].getNombre() + ' | ' + 'Edad: ' + this.paciente[i].getEdad() + ' | ' + 'Rut: ' + this.paciente[i].getRut() + ' | ' + 'Diagnostico: ' + this.paciente[i].getDiagnostico());
+    }
+};
+
+//Crear un método que permita mostrar todos los datos de los
+//usuarios registrados
+
+Consultorio.prototype.buscarDatosPorNombre = function (nombre) {
+    for (var i = 0; i < this.paciente.length; i++) {
+        if (nombre == this.paciente[i].getNombre()) {
+            console.log('Buscar paciente: ' + this.paciente[i].getNombre());
+            console.log('Nombre: ' + this.paciente[i].getNombre() + ' | ' + 'Edad: ' + this.paciente[i].getEdad() + ' | ' + 'Rut: ' + this.paciente[i].getRut() + ' | ' + 'Diagnóstico: ' + this.paciente[i].getDiagnostico());
+        }
+    }
+}
 
 //Instanciar cada objeto utilizando la instrucción new
-
-var consultorio = new Consultorio('Consultorio Medico Comunal'[pacienteUno, pacienteDos, pacienteTres, pacienteCuatro, pacienteCinco, pacienteSeis, pacienteSiete, pacienteOcho, pacienteNueve, pacienteDiez]);
 var pacienteUno = new Paciente('FRANCISCA ROJAS', 24, '9878782-1', 'COVID-19');
-var pacienteDos = new Paciente('PAMELA ESTRADA', 55, '15345241-3', 'DIABETES');
+var pacienteDos = new Paciente('PAMELA PAVEZ', 55, '15345241-3', 'DIABETES');
 var pacienteTres = new Paciente('ARMANDO LUNA', 36, '16445345-9', 'NEUMONIA');
 var pacienteCuatro = new Paciente('MANUEL GODOY', 15, '17666419-0', 'GRIPE');
-var pacienteCinco = new Paciente('RAMON ULLOA', 33, '14989389-K', 'HIPER TENSION');
+var pacienteCinco = new Paciente('RAMON ULLOA', 33, '14989389-K', 'HIPERTENSION');
 var pacienteSeis = new Paciente('PAULA SANCHEZ', 54, '15554774-5', 'COVID-19');
 var pacienteSiete = new Paciente('ANGÉLICA NAVAS', 26, '15444147-9', 'COVID-19');
 var pacienteOcho = new Paciente('ANA KLAPP', 45, '17879423-9', 'CEFALEA');
 var pacienteNueve = new Paciente('FELIPE MARDONES', 44, '1547423-6', 'DOLOR ABDOMINAL');
 var pacienteDiez = new Paciente('DIEGO MARRE', 51, '16554741-K', 'GRIPE');
+var consultorio1 = new Consultorio('Consultorio Medico Comunal', [pacienteUno, pacienteDos, pacienteTres, pacienteCuatro, pacienteCinco, pacienteSeis, pacienteSiete, pacienteOcho, pacienteNueve, pacienteDiez]);
 
-console.log(consultorio)
-console.log(pacienteDos)
-console.log(Paciente)
-
-
-
-
+consultorio1.mostrarPacientes();
+consultorio1.buscarDatosPorNombre('PAMELA PAVEZ');
